bd8a9c95cc1dc80ae1df50564722c209b0a7da31
Felipe Silva <felipejesusdasilva02@gmail.com>
2023-06-14 22:14:40 -0300

    update project

---
+++ src/main/java/br/com/lifetree/lifetreeTcc/LifetreeTccApplication.java
@@ -0,0 +1,13 @@
+package br.com.lifetree.lifetreeTcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifetreeTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifetreeTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lifetree/lifetreeTcc/LifetreeTccApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lifetree.lifetreeTcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifetreeTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
