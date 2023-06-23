58c0d38cfe17e12b287bf0f5f9c6c8dfbfd3dac1
FelipeMarani <felipejesusdasilva02@gmail.com>
2023-06-23 09:52:44 -0300

    update

--- src/main/java/br/com/lifetree/lifetreeTcc/repository/CupomRepository.java
+++ src/main/java/br/com/lifetree/lifetreeTcc/repository/CupomRepository.java
@@ -3,8 +3,10 @@ package br.com.lifetree.lifetreeTcc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.lifetree.lifetreeTcc.model.entity.Cupom;
+
 
 @Repository
-public interface CupomRepository extends JpaRepository<CupomRepository, Long> {
+public interface CupomRepository extends JpaRepository<Cupom, Long> {
 
 }
--- src/main/java/br/com/lifetree/lifetreeTcc/service/CupomService.java
+++ src/main/java/br/com/lifetree/lifetreeTcc/service/CupomService.java
@@ -1,5 +1,7 @@
 package br.com.lifetree.lifetreeTcc.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import br.com.lifetree.lifetreeTcc.model.entity.Cupom;
@@ -17,10 +19,16 @@ public class CupomService {
 		this.cupomRepository = _cupomService;
 	}
 	
-	//Metodo INSERT INTO CUPOM
+	//Metodo INSERT INTO 
 	@Transactional
 	public Cupom save(Cupom _cupom) {
 		return cupomRepository.save(_cupom);
 	}
-
+	
+	//METODO SELECT * FROM CLIENTE
+	public List<Cupom> findall(){
+		List<Cupom> lista = cupomRepository.findAll();
+		return lista;
+	}
+	
 }
