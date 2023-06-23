73f61a052c1e809d00c23ef5668187f85674f648
Diego0668 <diegolimaxd1@gmail.com>
2023-06-23 09:53:02 -0300

    update

--- src/main/java/br/com/lifetree/lifetreeTcc/repository/FuncionarioRepository.java
+++ src/main/java/br/com/lifetree/lifetreeTcc/repository/FuncionarioRepository.java
@@ -1,5 +1,11 @@
 package br.com.lifetree.lifetreeTcc.repository;
 
-public class FuncionarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.lifetree.lifetreeTcc.model.entity.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 
 }
--- src/main/java/br/com/lifetree/lifetreeTcc/service/FuncionarioService.java
+++ src/main/java/br/com/lifetree/lifetreeTcc/service/FuncionarioService.java
@@ -1,5 +1,30 @@
 package br.com.lifetree.lifetreeTcc.service;
 
-public class FuncionarioService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import br.com.lifetree.lifetreeTcc.model.entity.Funcionario;
+import br.com.lifetree.lifetreeTcc.repository.FuncionarioRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class FuncionarioService {
+	
+	final FuncionarioRepository funcionarioRepository;
+	
+	public FuncionarioService(FuncionarioRepository _funcionarioRepository) {
+		this.funcionarioRepository = _funcionarioRepository;
+	}
+	
+	@Transactional
+	public Funcionario save(Funcionario _funcionario) {
+		return funcionarioRepository.save(_funcionario);
+	}
+	
+	public List<Funcionario> findAll(){
+		List<Funcionario> lista = funcionarioRepository.findAll();
+		return lista;
+	}
+	
 }
