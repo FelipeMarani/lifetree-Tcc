88ec872a654fb9de14cf3d06ae6f9de9c62d0849
FelipeMarani <felipejesusdasilva02@gmail.com>
2023-08-24 12:19:11 -0300

    update

--- src/main/resources/static/js/AdicionarProduto.js
+++ src/main/resources/static/js/AdicionarProduto.js
@@ -1,24 +1,10 @@
-const inputFile = document.querySelector("#picture__input");
-const pictureImage = document.querySelector(".picture__image");
-const pictureImageTxt = "Choose an image";
-pictureImage.innerHTML = pictureImageTxt;
-
-inputFile.addEventListener("change", function (e) {
-  const inputTarget = e.target;
-  const file = inputTarget.files[0];
-
-  if (file) {
-    const reader = new FileReader();
-
-    reader.addEventListener("load", function (e) {
-      const readerTarget = e.target;
+/* Pré Visualização da Imagem Escolhida */
+function preViewImg() {
+	var imgPreView = new FileReader();
+	imgPreView.readAsDataURL(document.getElementById("uploadImage").files[0]);
 
 	imgPreView.onload = function(imgPreViewEvent) {
 		document.getElementById("preView").src = imgPreViewEvent.target.result;
-		document.getElementById("preView").style.width = '450px';
-		document.getElementById("preView").style.height = '450px';
-
 	}
-	
 }
   
