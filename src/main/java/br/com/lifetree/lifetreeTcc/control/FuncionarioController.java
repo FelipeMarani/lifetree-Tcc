7f1148ba0714ea0c8a0cf6e8c4059e5bf172621d
Diego0668 <diegolimaxd1@gmail.com>
2023-09-07 19:37:24 -0300

    update

--- src/main/java/br/com/lifetree/lifetreeTcc/control/FuncionarioController.java
+++ src/main/java/br/com/lifetree/lifetreeTcc/control/FuncionarioController.java
@@ -14,7 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.lifetree.lifetreeTcc.model.entity.Funcionario;
+import br.com.lifetree.lifetreeTcc.model.entity.Produto;
 import br.com.lifetree.lifetreeTcc.service.FuncionarioService;
+import br.com.lifetree.lifetreeTcc.service.McProdutoService;
+import br.com.lifetree.lifetreeTcc.service.ProdutoService;
+import br.com.lifetree.lifetreeTcc.service.TpProdutoService;
 
 @Controller
 @RequestMapping("/lifetree/funcionario")
@@ -22,13 +26,20 @@ import br.com.lifetree.lifetreeTcc.service.FuncionarioService;
 public class FuncionarioController {
 
 	// CRIAÇÃO DO OBJETO DE SERVIÇO 
+	final ProdutoService produtoService;
 	final FuncionarioService funcionarioService;
+	final TpProdutoService tpProdutoService;
+	final McProdutoService mcProdutoService;
 
 
 	//INJEÇÃO DE DEPENDENCIA 
 
-	public FuncionarioController(FuncionarioService _funcionarioService) {
+	public FuncionarioController(FuncionarioService _funcionarioService , TpProdutoService tpProdutoService,
+			McProdutoService mcProdutoService, ProdutoService produtoService) {
 		this.funcionarioService = _funcionarioService;
+		this.tpProdutoService = tpProdutoService;
+		this.mcProdutoService = mcProdutoService;
+		this.produtoService = produtoService;
 
 	}
 
@@ -54,7 +65,7 @@ public class FuncionarioController {
 		if(acessar == 2 ) {
 			return "redirect:/lifetree/produtos/Estoque";
 		}else if(acessar ==1) {
-			return "redirect:/lifetree/produtos/EditarFuncionario";
+			return "redirect:/lifetree/funcionario/EditarFuncionario";
 		}
 		
 		
@@ -63,6 +74,18 @@ public class FuncionarioController {
 
 	return "redirect:/lifetree/funcionario/login";
 	}
+@GetMapping("/AdicionarProdutoADM")
+public String getadicionarProdutosADM(ModelMap model) {
+	model.addAttribute("tpProdutos", tpProdutoService.findAll());
+	model.addAttribute("mcProdutos", mcProdutoService.findAll());
+	model.addAttribute("produto", new Produto());
+	return "AdicionarProdutoADM";
+}
+@GetMapping ("/EstoqueADM")
+public String getEstoque(ModelMap model){
+	model.addAttribute("produtos",  produtoService.ListarTodos());
+	return "EstoqueADM";
+} 
 
 	@PostMapping ("/save")
 	public String saveFuncionario(@ModelAttribute Funcionario funcionario) {
@@ -71,6 +94,19 @@ public class FuncionarioController {
 
 	return "redirect:/lifetree/funcionario/login";
 }
+	@PostMapping("/todos-filtro")
+	public String mostrarTodosFiltro(ModelMap model, 
+			@RequestParam(value = "nomeProd", required = false) String nome) {
+		// assim q eu edito o produto, ele esta gerando outro produto dublicado
+
+		if (nome.trim().equals("")) {
+			model.addAttribute("produtos", produtoService.ListarTodos());
+		} else {
+			model.addAttribute("produtos", produtoService.listarTodosFiltro(nome));
+		}
+		return "redirect:/lifetree/funcionario/EstoqueADM";
+	}
+
 
 	//ROTA GET
 	@GetMapping ("/all")
@@ -98,6 +134,7 @@ public class FuncionarioController {
 	public String getRecuperaçãoSenha() {
 		return "RecuperaçãoSenha";
 	}
+	
 
 	@GetMapping ("/EditarFuncionario")
 	public String getEditarFuncionario(ModelMap map){
---
+++ src/main/resources/static/js/AdicionarProdutoADM.js
@@ -0,0 +1,19 @@
+const inputFile = document.querySelector("#picture__input");
+const pictureImage = document.querySelector(".picture__image");
+const pictureImageTxt = "Escolha uma imagem para o seu produto";
+
+/* Pré Visualização da Imagem Escolhida */
+function preViewImg() {
+	var imgPreView = new FileReader();
+	imgPreView.readAsDataURL(document.getElementById("uploadImage").files[0]);
+
+	imgPreView.onload = function(imgPreViewEvent) {
+		document.getElementById("preView").src = imgPreViewEvent.target.result;
+		document.getElementById("preView").style.width = '450px';
+		document.getElementById("preView").style.height = '450px';
+
+	}
+	
+}
+  
+  
