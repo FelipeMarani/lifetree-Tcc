03cab59e9df41dfcf36476074da068d8a98d825c
FelipeMarani <felipejesusdasilva02@gmail.com>
2023-09-27 11:42:12 -0300

    update

--- src/main/java/br/com/lifetree/lifetreeTcc/control/FuncionarioApiController.java
+++ src/main/java/br/com/lifetree/lifetreeTcc/control/FuncionarioApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import br.com.lifetree.lifetreeTcc.model.entity.Funcionario;
 import br.com.lifetree.lifetreeTcc.model.entity.Produto;
 import br.com.lifetree.lifetreeTcc.service.FuncionarioService;
 import br.com.lifetree.lifetreeTcc.service.ProdutoService;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/lifetree/funcionario")
@@ -54,27 +56,43 @@ public class FuncionarioApiController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-
-	@PostMapping("/funcionarioApi")
-	public ResponseEntity<Funcionario> getOneFuncionario(@RequestParam(required = false) String title,
-			@RequestParam("email") String email, 
-			@RequestParam("senha") String senha,
-			@RequestBody Funcionario funcionario) {
-
+	
+	@PostMapping("/logar")
+	public ResponseEntity<Funcionario> Acessar(ModelMap map, @RequestParam("email") String email,
+			@RequestParam("senha") String senha, HttpSession session) {
 		try {
+			Funcionario funcionario = funcionarioService.acessar(email, senha);
 
-			Funcionario func = funcionarioService.acessar(email, senha);
-
-			if (func != null) {
-
-				return new ResponseEntity<Funcionario>(func, HttpStatus.OK);
-
+			if (funcionario != null) {
+				return new ResponseEntity<>(funcionario, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 			}
+
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+
+//	@PostMapping("/funcionarioApi")
+//	public ResponseEntity<Funcionario> getOneFuncionario(
+//			@RequestBody Funcionario funcionario) {
+//
+//		try {
+//			int resposta = funcionarioService.logarApp(funcionario.getEmail(), funcionario.getSenha());
+//
+//			if (resposta == 1) {
+//
+//				Funcionario f2 = funcionarioService.findByEmail(funcionario.getEmail());
+//
+//				return new ResponseEntity<Funcionario>(f2, HttpStatus.OK);
+//
+//			}
+//		} catch (Exception e) {
+//			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//		}
+//		return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//	}
 
 	@PostMapping("/produtosApi")
 	public ResponseEntity<List<Produto>> getAllProdutos(@RequestParam(required = false) String title2) {
